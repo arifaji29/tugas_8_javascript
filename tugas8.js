440df5a293d6607cb683f9135b5e832ce412dc88
Arif <arifajif@gmail.com>
2022-06-01 22:20:01 +0700

    tugas 8

---
+++ tugas8.js
@@ -0,0 +1,14 @@
+function panggilobject() {
+    var ikan = { 
+        nama : "lele",
+        harga : 20000,
+        habitat : "sungai",
+      
+    }
+    ikan.nama="patin";
+    ikan.warna="hitam";
+    console.log(ikan)
+    
+}
+
+panggilobject()
